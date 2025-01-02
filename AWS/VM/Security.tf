fda260742f2d6d1f255ad7cf0f89aaccfea925c5
Dimitrios Skrettas <121012738+dskretta@users.noreply.github.com>
2025-01-01 22:45:22 -0500

    Create Security.tf

---
+++ AWS/VM/Security.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "Final-Project" {
+  vpc_id = aws_vpc.Final-Project.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+
+  tags = {
+    name = "Final-Project-security-group"
+  }
+}
+
