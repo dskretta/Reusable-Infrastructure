e8ebe06a2a4718f42130d2ba5242ca2f4951b7e4
Dimitrios Skrettas <121012738+dskretta@users.noreply.github.com>
2025-01-01 23:17:17 -0500

    Create Security.tf

---
+++ AZURE/Security.tf
@@ -0,0 +1,26 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  depends_on = [azurerm_subnet.subnet] # NSG depends on the subnet
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+
+  depends_on = [azurerm_network_security_group.nsg, azurerm_subnet.subnet]
+}
